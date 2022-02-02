f116625faa60f3c35ffee843f73086305d6ed49b
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-02 12:51:42 +0530

    02-02-2022 : fix crash

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
+const reducer = combineReducers({});
+
 let initialState = {}
 
 const middleware = [thunk];
